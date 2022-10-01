8bc7121f5f8d16541960d2910c47fd50e15c7470
Evnikel <evnikel@gmail.com>
2022-10-01 22:23:00 +0300

    added all ckass

---
+++ src/test/java/Login.java
@@ -0,0 +1,50 @@
+
+import model.User;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class Login extends TestBase {
+
+
+
+    @BeforeTest
+    public void preConditions() {
+        if (app.getUser().isLogged()) {
+            app.getUser().logOut();
+        }
+    }
+
+
+
+    @Test
+    public void loginPositive1() {
+        User user = new User().withEmail("evnikel@gmail.com").withPassword("EB106201!");
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm(user);
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+    }
+
+    @Test
+    public void loginPositive2() {
+
+        app.getUser().initLogin();
+        app.getUser().pause(2000);
+        app.getUser().fillLoginForm2("evnikel@gmail.com","EB106201eb!");
+        app.getUser().submitLogin();
+        app.getUser().pause(2000);
+
+
+        Assert.assertTrue(app.getUser().isLogged());
+
+
+    }
+
+}
+
